ef83886b37a34069e2bb8cdcfa9386c9f57b4132
DanielDamasc <dandamasceno04@gmail.com>
2026-02-28 16:03:19 -0300

    feat: dark mode na tela de ar-condicionado

--- app/Livewire/AirConditioningTable.php
+++ app/Livewire/AirConditioningTable.php
@@ -142,12 +142,12 @@ final class AirConditioningTable extends PowerGridComponent
         return [
             Button::add('edit')
                 ->slot(Blade::render('<x-heroicon-o-pencil-square class="w-5 h-5" />'))
-                ->class('text-secondary-600 hover:text-secondary-800 p-1 mr-2 transition-colors')
+                ->class('p-1 transition-colors text-secondary-400 hover:text-secondary-600 dark:text-secondary-400 dark:hover:text-secondary-300')
                 ->dispatchTo('air-conditioners-manager', 'open-edit', ['id' => $row->id]),
 
             Button::add('delete')
                 ->slot(Blade::render('<x-heroicon-o-trash class="w-5 h-5" />'))
-                ->class('text-red-600 hover:text-red-800 p-1 transition-colors')
+                ->class('p-1 transition-colors text-red-400 hover:text-red-600 dark:text-red-400 dark:hover:text-red-300')
                 ->dispatchTo('air-conditioners-manager', 'confirm-delete', ['id' => $row->id]),
         ];
     }
